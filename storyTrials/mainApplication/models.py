53b6820cc2f4450b2511093dbcf35e53940f2e5e
bernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-06-14 16:01:50 -0300

    :sparkles: feat: Created create user endpoint

--- storyTrials/mainApplication/models.py
+++ storyTrials/mainApplication/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
+import uuid 
+
 class User(models.Model):
-    id=models.UUIDField(unique=True, db_index=True, primary_key=True,auto_created=True)
+    id=models.UUIDField(default = uuid.uuid4, editable = False, db_index=True, primary_key=True,auto_created=True)
     userName = models.CharField(max_length=255)
     userLogin = models.CharField(max_length=255, unique=True)
     userPassword = models.CharField(max_length=255)
-    
+
     def __str__(self):
         return self.userName
 
 class Collection(models.Model):
-    id=models.UUIDField(unique=True, db_index=True, primary_key=True, auto_created=True, )
+    id=models.UUIDField( default = uuid.uuid4,  editable = False, db_index=True, primary_key=True, auto_created=True, )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     collectionName = models.CharField(max_length=255)
     collectionObjective = models.IntegerField()
@@ -18,7 +20,7 @@ class Collection(models.Model):
         return self.collectionName
     
 class Book(models.Model):
-    id=models.UUIDField(unique=True, db_index=True, primary_key=True, auto_created=True, )
+    id=models.UUIDField( default = uuid.uuid4, editable = False, db_index=True, primary_key=True, auto_created=True, )
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     bookName = models.CharField(max_length=255)
     pagesAmount = models.IntegerField()
--- storyTrials/mainApplication/urls.py
+++ storyTrials/mainApplication/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('users/find/all', views.getAllUsers, name="findAllUsers"),
     path('users/find/<str:id>', views.getById, name="findUserById"),
+    path("users/create", views.createUser, name="createUser")
+    
 ]
