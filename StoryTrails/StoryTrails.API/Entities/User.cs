7c0437efdc1b37ca27870329d8ccc9799b0655ed
bernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-07-01 09:34:51 -0300

    :sparkles: Feat: Created user entity

---
+++ StoryTrails/StoryTrails.API/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace StoryTrails.API.Entities
+{
+    public class User
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Username { get; set; } = string.Empty;
+
+        public string UserLogin { get; set; } = string.Empty;
+
+        public string UserPassword { get; set; } = string.Empty;
+     }
+}
