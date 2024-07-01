522e32e3567f23c851b307f7b767881fb5774de2
bernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-07-01 09:36:39 -0300

    :sparkles: Feat: Created Collection Entity

---
+++ StoryTrails/StoryTrails.API/Entities/Collection.cs
@@ -0,0 +1,12 @@
+﻿namespace StoryTrails.API.Entities
+{
+    public class Collection
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string CollectionName { get; set; } = string.Empty;
+
+        public int CollectionObjective { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+    }
+}
