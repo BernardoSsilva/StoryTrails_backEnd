bb7b1c21f305fdf9c36d970e0beadd37039b3105
Bernardo <126777966+BernardoSsilva@users.noreply.github.com>
2024-07-01 08:47:48 -0300

    Delete storyTrails directory

--- storyTrails/application/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book, Collection, User
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Collection)
-
-admin.site.register(Book)
